53539993d5d7d980a29c45f37aff1ded9747c203
Balazs Kelemen <64102143+kelbalazs@users.noreply.github.com>
2023-03-20 13:49:24 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <PatchMeta />
+  <NavBar
+    :title="'📝 vue3-md-blog'"
+    :sections="blogSections"
+  />
+  <Suspense>
+    <template #default>
+      <router-view />
+    </template>
+    <template #fallback>
+      <Loader />
+    </template>
+  </Suspense>
+  <Footer />
+</template>
+
+<script setup lang="ts">
+import { inject } from 'vue'
+import PatchMeta from './components/PatchMeta.vue'
+import Loader from './components/Loader.vue'
+import NavBar from './components/NavBar.vue'
+import Footer from './components/Footer.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+
+const blogSections = inject('blogSections', {})
+</script>
